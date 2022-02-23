e7b98b0f76ade51b70bdf7219a574a3cad3a457d
trainb0y1 <66213737+trainb0y1@users.noreply.github.com>
2022-02-22 20:37:38 -0700

    Move grounded check to PlayerController.cs

--- Assets/JumpRefresh.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-public class JumpRefresh : MonoBehaviour
-{
-    public PlayerController player;
-    void FixedUpdate() {
-        if (Physics2D.OverlapPointAll(transform.position).Length > 0) player.jumpState = JumpState.GROUNDED;
-        else if (player.jumpState != JumpState.JUMPING) player.jumpState = JumpState.AIRBORNE ; // reset jump state
-    }
-}
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -16,6 +16,12 @@ public class PlayerController : MonoBehaviour
 
 	void FixedUpdate()
 	{
+		// Handle resetting jump
+		Vector2 checkPos = new Vector2(transform.position.x, transform.position.y - ((transform.localScale.y / 2)) - 0.1f);
+		// checkPos is the position to check whether it is in the ground. hopefully this works for all sizes
+		if (Physics2D.OverlapPointAll(checkPos).Length > 0) jumpState = JumpState.GROUNDED;
+        else if (jumpState != JumpState.JUMPING) jumpState = JumpState.AIRBORNE ; // reset jump state
+
 		if (Input.GetKey(KeyCode.A)) Move(new Vector2(-acceleration * Time.fixedDeltaTime, 0));
 		if (Input.GetKey(KeyCode.D)) Move(new Vector2(acceleration * Time.fixedDeltaTime, 0));
 		if (Input.GetKey(KeyCode.W) && jumpState != JumpState.AIRBORNE)  Jump(); 
