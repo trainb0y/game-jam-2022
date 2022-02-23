c28c49b02ce00cace0475319db28fe05a2e93faf
trainb0y1 <66213737+trainb0y1@users.noreply.github.com>
2022-02-22 20:30:31 -0700

    Better jumping

---
+++ Assets/JumpState.cs
@@ -0,0 +1,5 @@
+public enum JumpState {
+    GROUNDED,
+    JUMPING,
+    AIRBORNE
+}
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,26 +4,41 @@ public class PlayerController : MonoBehaviour
 {
 	public float acceleration = 10;
 	public float velocityCap = 3;
-	public float airborneAccelMultiplier = 0.5f;
+	public float airbornAccelMultiplier = 0.5f;
 	public float jumpForce = 100;
+	public float jumpBoostForce = 5;
+	public int jumpBoostMax = 20;
+	public JumpState jumpState = JumpState.GROUNDED;
 	private Rigidbody2D rb;
-	public bool grounded = true;
+	private int jumpCount = 0;
 
 	void Awake(){rb = GetComponent<Rigidbody2D>();}
 
-	void Update()
+	void FixedUpdate()
 	{
-		if (Input.GetKey(KeyCode.A)) Move(new Vector2(-acceleration * Time.deltaTime, 0));
-		if (Input.GetKey(KeyCode.D)) Move(new Vector2(acceleration * Time.deltaTime, 0));
-		if (Input.GetKey(KeyCode.W) && grounded)  {
-			rb.AddForce(new Vector2(0, jumpForce * Time.deltaTime));
-			grounded = false;
-		}
+		if (Input.GetKey(KeyCode.A)) Move(new Vector2(-acceleration * Time.fixedDeltaTime, 0));
+		if (Input.GetKey(KeyCode.D)) Move(new Vector2(acceleration * Time.fixedDeltaTime, 0));
+		if (Input.GetKey(KeyCode.W) && jumpState != JumpState.AIRBORNE)  Jump(); 
 	}
 
 	void Move(Vector2 accel) {
 		if (rb.velocity.magnitude > velocityCap && Mathf.Sign(accel.x) == Mathf.Sign(rb.velocity.x)) return;
-		if (!grounded) accel *= airborneAccelMultiplier;
+		if (jumpState != JumpState.GROUNDED) accel *= airbornAccelMultiplier;
 		rb.AddForce(accel);
+	}
+
+	void Jump(){
+		if (jumpState == JumpState.JUMPING) {
+			if (jumpCount > jumpBoostMax) {
+				jumpState = JumpState.AIRBORNE;
+			}
+			rb.AddForce(new Vector2(0, jumpBoostForce * Time.fixedDeltaTime));
+			jumpCount++;
+		}
+		if (jumpState == JumpState.GROUNDED) {
+			rb.AddForce(new Vector2(0, jumpForce * Time.fixedDeltaTime));
+			jumpState = JumpState.JUMPING;
+			jumpCount = 0;
+		}
 	}
 }
