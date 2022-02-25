a787bb341c715ca376104687434ed64a2ce19be2
trainb0y1 <66213737+trainb0y1@users.noreply.github.com>
2022-02-25 13:39:07 -0700

    Let platforms pause at each end

--- Assets/MovingPlatform.cs
+++ Assets/MovingPlatform.cs
@@ -7,8 +7,10 @@ public class MovingPlatform : MonoBehaviour
     public Vector3 startPos;
     public Vector3 endPos;
     public float speed;
-
+    public int waitTime;
     private Vector3 destination;
+
+    private float counter = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,8 +23,12 @@ public class MovingPlatform : MonoBehaviour
     {
         transform.position = Vector3.MoveTowards(transform.position, destination, speed*Time.deltaTime);
         if (Util.Vector3DistanceSquared(transform.position, destination) < 0.2f) {
-            if (endPos == destination) destination = startPos;
-            else destination = endPos;
+            counter += Time.deltaTime;
+            if (counter > waitTime) {
+                counter = 0;
+                if (endPos == destination) destination = startPos;
+                else destination = endPos;
+            }
         }
     }
 
