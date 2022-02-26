cc9b1ecebe1f3d8e52a639b6ddc53d943a2dbef0
trainb0y1 <66213737+trainb0y1@users.noreply.github.com>
2022-02-25 18:11:21 -0700

    Outline!

--- Assets/Scripts/LevelHandler.cs
+++ Assets/Scripts/LevelHandler.cs
@@ -6,8 +6,10 @@ public class LevelHandler : MonoBehaviour
 {
     public Transform spawnPoint;
     public int minY = -10;
-    public int particleDensity = 10;
+    public int breakParticleDensity = 10;
     public GameObject breakParticlePrefab;
+    public int outlineParticleDensity = 10;
+    public GameObject outlineParticlePrefab;
     // Yes, we could just use a dictionary but unity doesn't like dictionaries
     public LevelColor[] level;
     public LevelColor currentColor;
@@ -22,12 +24,13 @@ public class LevelHandler : MonoBehaviour
         player = FindObjectOfType<PlayerController>().gameObject;
         foreach (LevelColor lc in level) {
             foreach (GameObject o in lc.objects) {
-                
-                GameObject particleObject = Instantiate(breakParticlePrefab);
-                particleObject.name = breakParticlePrefab.name; // avoid the "(clone)" part
-                particleObject.transform.parent = o.transform;
-                particleObject.transform.localScale = new Vector3(1, 1, 1);
-                particleObject.transform.localPosition = new Vector3(0, 0, 0);
+                foreach (GameObject prefab in new GameObject[]{breakParticlePrefab, outlineParticlePrefab}) {
+                    GameObject particleObject = Instantiate(prefab);
+                    particleObject.name = prefab.name; // avoid the "(clone)" part
+                    particleObject.transform.parent = o.transform;
+                    particleObject.transform.localScale = new Vector3(1, 1, 1);
+                    particleObject.transform.localPosition = new Vector3(0, 0, 0);
+                }
                 
                 o.GetComponent<SpriteRenderer>().color = lc.color;
                 Disable(o);
@@ -61,28 +64,45 @@ public class LevelHandler : MonoBehaviour
     }
 
     void Enable(GameObject o) {
+        EndOutlineParticles(o);
         o.GetComponent<SpriteRenderer>().enabled = true;
         foreach (BoxCollider2D collider in o.GetComponents<BoxCollider2D>()) collider.enabled = true;
-        if (o.GetComponent<MovingPlatform>() != null) {
-            player.transform.parent = null;
-            o.SetActive(true);
-        }
+        if (o.GetComponent<MovingPlatform>() != null) player.transform.parent = null;
         BeginGrowing(o);
     }
     void Disable(GameObject o) {
         o.GetComponent<SpriteRenderer>().enabled = false;
         foreach (BoxCollider2D collider in o.GetComponents<BoxCollider2D>()) collider.enabled = false;
+        if (o.GetComponent<MovingPlatform>() != null) {
+            player.transform.parent = null;
+        }
+        SpawnDisableParticles(o);
+        StartOutlineParticles(o);
+    }
+
+    void SpawnDisableParticles(GameObject o) {
         ParticleSystem particles = o.transform.Find(breakParticlePrefab.name).gameObject.GetComponent<ParticleSystem>();
         var main = particles.main;
         var emission = particles.emission;
         main.startColor = Util.InvertColor(currentColor.color); // hacky solution. might cause issues if there are more than just black/white
         var burst = emission.GetBurst(0);
-        burst.count = o.transform.lossyScale.x * o.transform.lossyScale.y * particleDensity;
+        burst.count = o.transform.lossyScale.x * o.transform.lossyScale.y * breakParticleDensity;
         emission.SetBurst(0, burst);
         particles.Play();
-        if (o.GetComponent<MovingPlatform>() != null) {
-            player.transform.parent = null;
-        }
+    }
+
+    void StartOutlineParticles(GameObject o) {
+        ParticleSystem particles = o.transform.Find(outlineParticlePrefab.name).gameObject.GetComponent<ParticleSystem>();
+        var main = particles.main;
+        var emission = particles.emission;
+        emission.rateOverTime = outlineParticleDensity * ((transform.lossyScale.x * 2) + (transform.lossyScale.y * 2));
+        main.startColor = Util.InvertColor(currentColor.color); // hacky solution. might cause issues if there are more than just black/white
+        particles.Play();
+    }
+
+    void EndOutlineParticles(GameObject o) {
+        ParticleSystem particles = o.transform.Find(outlineParticlePrefab.name).gameObject.GetComponent<ParticleSystem>();
+        particles.Stop();
     }
 
     public void ChangeColor(LevelColor color) {
