edfa4081baab9f9f53161d2061ce8bc9a8d39e94
trainb0y1 <66213737+trainb0y1@users.noreply.github.com>
2022-02-26 12:32:17 -0700

    Main Menu

--- Assets/Scripts/EndPoint.cs
+++ Assets/Scripts/EndPoint.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndPoint : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.GetComponent<PlayerController>() != null) {
             // move to next scene
-            Debug.Log("End point triggered");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
